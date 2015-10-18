a4c69aebb66dcbe71d0cec177c2015845863552a
luoye_lj <luoye.lj@sohu.com>
2015-10-18 18:25:23 +0800

    个人后台，用户中心和文件中心弄好了

---
+++ test/unit/com/lj/tps/tags/CustomizeTagTagLibTests.groovy
@@ -0,0 +1,17 @@
+package com.lj.tps.tags
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CustomizeTagTagLib)
+class CustomizeTagTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
